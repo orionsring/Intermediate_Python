defee4dbe3cdeb5107fdfe279f5917d8d07f339b
Donny <orionsring@tamu.edu>
2019-04-02 17:15:37 -0700

    Add files via upload

--- test_phonebook.py
+++ test_phonebook.py
@@ -4,17 +4,21 @@ import unittest
 from phonebook import Phonebook
 
 class PhonebookTest(unittest.TestCase):
-
-    def test_create_phonebook(self):
-	    phonebook = Phonebook()
+    
+	def setUp(self):
+	    self.phonebook = Phonebook()
 		
 	def test_lookup_entry_by_name(self):
-	    phonebook = Phonebook()
-		phonebook.add("Bob", "123445")
-		self.assertEqual("12345", phonebook.lookup("Bob"))
+	    self.phonebook.add("Bob", "123445")
+		self.assertEqual("12345", self.phonebook.lookup("Bob"))
 	
     def test_missing_entry_raises_keyError(self):
+	    with self.assertRaises(KeyError):
+            self.phonebook.lookup("missing")
+	
+	@unittest.skip("WIP") # work in progress
+	def test_empty_phonebook_is_consistent(self):
 	    phonebook = Phonebook()
-		with self.assertRaises(KeyError):
-            phonebook.lookup("missing")
+		self.assertTrue(self.phonebook.is_consistent())
+		
 		
