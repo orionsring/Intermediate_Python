224f017cb198e5bc03171099bc10ffc658d4d050
Donny <orionsring@tamu.edu>
2019-04-07 14:54:53 -0700

    Add files via upload

--- phonebook.py
+++ phonebook.py
@@ -1,7 +1,11 @@
+import os
 
 class Phonebook:
-    def __init__(self):
+
+    def __init__(self, cachedir):
 	    self.entries = {}
+		self.filename = "phonebook.txt"
+		self.file_cache = open(os.path.join(str(cachedir), self,filename), "w")
 		
 	def add(self, name, number):
 	    self.entries[name] = number
@@ -9,4 +13,14 @@ class Phonebook:
 	def lookup(self, name):
 	    return self.entries[name]
 		
+	def names(self):
+	    return self.entries.keys()
+		
+	def numbers(self):
+	    return self.entries.values()
+		
+	def clear(self):
+	    self.entries = {}
+		self.file_cache.close()
+		os.remove(self.filename)
 		
